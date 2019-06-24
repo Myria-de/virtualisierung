de7d9bd70839ef5823fa56c34e2a4e273bb05e27
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2019-06-24 23:08:25 +0100

    Konfiguration von Mac OS in Virtualbox

---
+++ macos_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Code for Mac OS in Virtualbox:
+echo "Starte VM-Konfiguration MacOS"
+vboxmanage modifyvm "MacOS" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+vboxmanage setextradata "MacOS" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
+vboxmanage setextradata "MacOS" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+vboxmanage setextradata "MacOS" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
+vboxmanage setextradata "MacOS" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+vboxmanage setextradata "MacOS" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+vboxmanage setextradata "MacOS" "VBoxInternal2/EfiGraphicsResolution" "1024x786"
+# oder beispielsweise # vboxmanage setextradata "MacOS" "VBoxInternal2/EfiGraphicsResolution" "1920x1080"
+echo "VM-Konfiguration beendet"
